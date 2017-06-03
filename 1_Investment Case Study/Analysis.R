bfb750494cfe8f430004239de43678f3cdc4a705
Anupam Majhi <majhi.anupam@gmail.com>
2017-06-03 06:53:20 +0530

    Fix line

--- 1_Investment Case Study/Analysis.R
+++ 1_Investment Case Study/Analysis.R
@@ -59,3 +59,17 @@ mean(master_frame$raised_amount_usd[which(master_frame$funding_round_type == "pr
 
 # Filter by chosen investment type
 filter_by_inv_type <- filter(master_frame,funding_round_type == "venture")
+# Group by country code
+group_by_country <- group_by(filter_by_inv_type,country_code)
+# Find sum of investment based on country code
+total_investment_by_country <- summarise(group_by_country,Total_Investment = sum(raised_amount_usd,na.rm = T))
+# For the chosen investment type, make a data frame named top9 with the top nine countries (based on the total investment amount each country has received)
+# Also cleanup Blank Country Code
+top_9 <- arrange(total_investment_by_country[which(total_investment_by_country$country_code != ""),],desc(Total_Investment))[1:9,]
+
+
+#============ GOAL 3 : Sector analysis ============================
+
+# Checkpoint 4: Sector Analysis 1
+
+mapping_data_raw <- read.csv("mapping.csv",check.names=FALSE,stringsAsFactors = F)
