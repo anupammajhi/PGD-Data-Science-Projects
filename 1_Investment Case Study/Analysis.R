57d39a156b6f2815c9140a10bb30d73462e0b980
Anupam Majhi <majhi.anupam@gmail.com>
2017-06-04 06:10:36 +0530

    Update and commit

--- 1_Investment Case Study/Analysis.R
+++ 1_Investment Case Study/Analysis.R
@@ -130,3 +130,21 @@ Top_Sector_D3 <- arrange(distinct(D3[c("main_sector","total_investment_by_number
 Sec_Sector_D1 <- arrange(distinct(D1[c("main_sector","total_investment_by_number")]),desc(total_investment_by_number))[2,1]
 Sec_Sector_D2 <- arrange(distinct(D2[c("main_sector","total_investment_by_number")]),desc(total_investment_by_number))[2,1]
 Sec_Sector_D3 <- arrange(distinct(D3[c("main_sector","total_investment_by_number")]),desc(total_investment_by_number))[2,1]
+
+# Third Sector name (no. of investment-wise)
+Thr_Sector_D1 <- arrange(distinct(D1[c("main_sector","total_investment_by_number")]),desc(total_investment_by_number))[3,1]
+Thr_Sector_D2 <- arrange(distinct(D2[c("main_sector","total_investment_by_number")]),desc(total_investment_by_number))[3,1]
+Thr_Sector_D3 <- arrange(distinct(D3[c("main_sector","total_investment_by_number")]),desc(total_investment_by_number))[3,1]
+
+# Number of investments in top sector (3)
+arrange(distinct(D1[c("main_sector","total_investment_by_number")]),desc(total_investment_by_number))[1,2]
+arrange(distinct(D2[c("main_sector","total_investment_by_number")]),desc(total_investment_by_number))[1,2]
+arrange(distinct(D3[c("main_sector","total_investment_by_number")]),desc(total_investment_by_number))[1,2]
+
+# Number of investments in second sector (4)
+arrange(distinct(D1[c("main_sector","total_investment_by_number")]),desc(total_investment_by_number))[2,2]
+arrange(distinct(D2[c("main_sector","total_investment_by_number")]),desc(total_investment_by_number))[2,2]
+arrange(distinct(D3[c("main_sector","total_investment_by_number")]),desc(total_investment_by_number))[2,2]
+
+# Number of investments in third sector (5)
+arrange(distinct(D1[c("main_sector","total_investment_by_number")]),desc(total_investment_by_number))[3,2]
