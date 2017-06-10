634c67217ae209c7bfe87a97b56d2d245fab662d
Anupam Majhi <majhi.anupam@gmail.com>
2017-06-10 07:50:26 +0530

    Code change

--- 2_Uber Case Study/UberCaseStudy.R
+++ 2_Uber Case Study/UberCaseStudy.R
@@ -16,3 +16,15 @@ UberData <- read.csv("Uber Request Data.csv")
 
   # All rows should have Request Time Stamp, Pickup point and Request ID
   sum(is.na(UberData$Request.timestamp)) #No missing data
+  sum(is.na(UberData$Request.id)) #No missing data
+  sum(is.na(UberData$Pickup.point)) #No missing data
+  
+  # Check Similar values
+  levels(UberData$Status) #All distinct values, unambiguous, No need to lower case
+  levels(UberData$Pickup.point) #All distinct values, unambiguous, No need to lower case
+
+  # Request.id and Pickup.point should not have any missing Values
+  sum(is.na(UberData$Request.id)) #No missing values
+  sum(is.na(UberData$Pickup.point)) #No missing values
+
+  # Completed Trips should have Both Pickup and Drop Time stamps and Driver ID
