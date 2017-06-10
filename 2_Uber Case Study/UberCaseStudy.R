b3afa3b5e193b8715a0852e5bd26482a76cb6775
Anupam Majhi <majhi.anupam@gmail.com>
2017-06-10 07:28:20 +0530

    Quick Fix

---
+++ 2_Uber Case Study/UberCaseStudy.R
@@ -0,0 +1,18 @@
+# Remove all variables
+rm(list=ls())
+
+library(dplyr)
+
+# Read CSV File
+UberData <- read.csv("Uber Request Data.csv")
+
+#======= Data Cleaning and Preparation ========
+
+# Check duplicate values
+
+  sum(duplicated(UberData$Request.id)) #No Duplicates
+
+# Checking and validating missing values
+
+  # All rows should have Request Time Stamp, Pickup point and Request ID
+  sum(is.na(UberData$Request.timestamp)) #No missing data
