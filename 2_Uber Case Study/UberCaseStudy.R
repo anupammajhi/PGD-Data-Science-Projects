56acf79e94aab7dfaa820eb60db4ee0948f36963
Anupam Majhi <majhi.anupam@gmail.com>
2017-06-16 10:08:58 +0530

    Fix line

--- 2_Uber Case Study/UberCaseStudy.R
+++ 2_Uber Case Study/UberCaseStudy.R
@@ -432,3 +432,4 @@ UberData <- read.csv("Uber Request Data.csv")
       UberData[which(UberData$Status == 'No Cars Available'),] %>%
         group_by(HourOfTheDay) %>%
         summarise(NonAvailability = length(HourOfTheDay)) %>%
+        summarise(Average_Non_Availabilty = mean(NonAvailability))
