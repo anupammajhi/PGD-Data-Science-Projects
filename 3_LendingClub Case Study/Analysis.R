6103e5a35277616e408be6d9beff8aa57b73c39d
Anupam Majhi <majhi.anupam@gmail.com>
2017-06-26 05:38:52 +0530

    Changes in code

--- 3_LendingClub Case Study/Analysis.R
+++ 3_LendingClub Case Study/Analysis.R
@@ -35,3 +35,24 @@ loanData <-loanData[rowSums(is.na(loanData)) != ncol(loanData),]
 Vague_Columns <- names(which(lapply(loanData,function(x){length(levels(as.factor(x)))}) == 1))
 Vague_Columns
 
+# url: no new data that can be obtained
+Vague_Columns <- append(Vague_Columns,"url")
+
+# id and member_id
+loanData[which(duplicated(loanData$id)),] # 0 Duplicates, hence unique
+loanData[which(duplicated(loanData$member_id)),]  # 0 Duplicates, hence unique
+# Since both id and member_id are unique, we will exclude member_id
+
+# funded_amnt_inv, out_prncp_inv, total_pymnt_inv : Not required for this analysis as we are not analysing investor's action
+# desc, purpose and title : purpose is a better categorical variable for this study, as desc and title are quite random. Hence just use purpose and remove others.
+# emp_title : not relevant for this analysis
+Vague_Columns <- append(Vague_Columns,c("member_id","funded_amnt_inv","out_prncp_inv","total_pymnt_inv",
+                                        "desc","title","emp_title"))
+# last_credit_pull_d,next_pymnt_d,last_pymnt_amnt,last_pymnt_d,collection_recovery_fee,recoveries,out_prncp : not relevant for this analysis, as these variables are helpful only post default or full payment
+Vague_Columns <- append(Vague_Columns,c("last_credit_pull_d","next_pymnt_d","last_pymnt_amnt","last_pymnt_d","collection_recovery_fee","recoveries","out_prncp"))
+
+# Removing unnecessary columns from dataframe saved as list "Vague_Columns"
+loanData <- loanData[,!names(loanData) %in% Vague_Columns]
+
+#---- .. Data Consistency .. ----  
+
