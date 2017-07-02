a0db33207c57c31f8e064d281d1e50647bb848e3
Anupam Majhi <majhi.anupam@gmail.com>
2017-07-02 06:41:55 +0530

    Line update

--- 3_LendingClub Case Study/Analysis.R
+++ 3_LendingClub Case Study/Analysis.R
@@ -453,6 +453,3 @@ loanData %>%
   ggplot(aes(y=revol_util)) +
   geom_boxplot(aes(x=loan_status),width=0.6)+
   stat_summary(geom="text", fun.y=quantile,aes(x=loan_status,label=sprintf("%1.1f", ..y..)),position=position_nudge(x=0.2), size=3.5,vjust = -0.5)
-#higher the utilization of revolving balance, the borrower tends to default much more
-
-
