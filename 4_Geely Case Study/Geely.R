200922ce42871cc110f0433101c895c2a4569f5d
Anupam Majhi <majhi.anupam@gmail.com>
2017-07-16 05:31:13 +0530

    Changes in code

--- 4_Geely Case Study/Geely.R
+++ 4_Geely Case Study/Geely.R
@@ -15,3 +15,18 @@ carsDF$company <- as.vector(sapply(as.vector(carsDF$CarName), function(x){strspl
 levels(as.factor(carsDF$company))
 
 # Correcting company names
+carsDF[which(carsDF$company == "maxda"),"company"] <- "mazda"
+carsDF[which(carsDF$company == "Nissan"),"company"] <- "nissan"
+carsDF[which(carsDF$company == "porcshce"),"company"] <- "porsche"
+carsDF[which(carsDF$company == "toyouta"),"company"] <- "toyota"
+carsDF[which(carsDF$company == "vokswagen" | carsDF$company == "vw"),"company"] <- "volkswagen"
+carsDF[which(carsDF$company == "alfa-romero"),"company"] <- "alfa-romeo"
+
+# Checking NA
+which(is.na(carsDF)) #No NA Value found
+
+# Ignoring unwanted columns
+carsDF <- carsDF[,!names(carsDF) %in% c("car_ID","CarName")]
+str(carsDF)
+
+
