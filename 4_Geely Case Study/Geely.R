c6bc6765cf4c2a0a6a25a1d22bd0bea1ab4cbc6a
Anupam Majhi <majhi.anupam@gmail.com>
2017-07-27 05:23:20 +0530

    Update

--- 4_Geely Case Study/Geely.R
+++ 4_Geely Case Study/Geely.R
@@ -555,5 +555,3 @@ sort(vif(model_33))
 # Model 34 : after removing citympg
 model_34 <- lm(price~enginelocation+wheelbase+horsepower+peakrpm+carbodyhardtop+carbodyhatchback+carbodysedan+carbodywagon+
                  companyaudi+companybmw+companybuick+companyjaguar+companymazda+companyporsche+hp2wRatio,
-               data = cars.Train)
-summary(model_34)
