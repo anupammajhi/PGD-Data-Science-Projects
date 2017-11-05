92725f926166d3e8b7bc85a2f564e6a363df5168
Anupam Majhi <majhi.anupam@gmail.com>
2017-11-05 06:58:02 +0530

    Fix line

--- 6_MNIST SVM/Assignment_2.R
+++ 6_MNIST SVM/Assignment_2.R
@@ -4,3 +4,12 @@ rm(list = ls())
 
 library(kernlab)
 library(readr)
+library(lattice)
+library(ggplot2)
+library(caret)
+library(e1071)
+
+#===== Data Preparation and Cleaning =====
+
+# Importing csv train file 
+# With no header as the file contains no header
