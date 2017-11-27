e51ea9f70297fbe74f1adf869567204f05a9dbc3
Anupam Majhi <majhi.anupam@gmail.com>
2017-11-27 06:44:41 +0530

    Line update

--- 7_Retail Sales Forecasting/main.R
+++ 7_Retail Sales Forecasting/main.R
@@ -902,3 +902,40 @@ for (i in seq(w,1,1)) {
 n <- length(eus_ts)
 diff <- eus_smooth[n-w] - eus_smooth[n-w-1]
 for (i in seq(n-w+1, n)) {
+    eus_smooth[i] <- eus_smooth[i-1] + diff
+}
+
+#Plot the smoothed time series
+
+timevals_in <- eus_in$Months
+
+lines(eus_smooth, col="yellow", lwd=2)
+
+
+#Trying Holt Winters
+
+plot(eus_ts)
+
+cols <- c("red", "blue", "green", "black", "grey")
+alphas <- c(0.02, 0.1, 0.3,0.5,0.8)
+labels <- c(paste("alpha =", alphas), "Original")
+for (i in seq(1,length(alphas))) {
+    eus_smoothhw <- HoltWinters(eus_ts, alpha=alphas[i],
+                                beta=FALSE, gamma=FALSE)
+    
+    lines(fitted(eus_smoothhw)[,1], col=cols[i], lwd=2)
+}
+
+legend("bottomleft", labels, col=cols, lwd=2)
+
+plot(eus_ts)
+eus_smoothhw <- HoltWinters(eus_ts, alpha=0.6,
+                            beta=FALSE, gamma=FALSE)
+
+lines(fitted(eus_smoothhw)[,1], col='red', lwd=2)
+
+
+# Again, Moving average does better smoothing as compared to Holt Winter, so we will use Moving Average smoothing.
+
+
+#Building a model on the smoothed time series using classical decomposition
