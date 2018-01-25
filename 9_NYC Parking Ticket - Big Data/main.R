e4552d4a6d950b46dbc707719d82fd2ca36f520f
Anupam Majhi <majhi.anupam@gmail.com>
2018-01-25 06:21:40 +0530

    Fix line

---
+++ 9_NYC Parking Ticket - Big Data/main.R
@@ -0,0 +1,17 @@
+
+# load SparkR
+library(SparkR)
+library(magrittr)
+library(ggplot2)
+
+# Initialize spark session
+sparkR.session(master='local')
+
+###################################################################################################################
+########################################## Data Preparation and Cleaning ##########################################
+
+# Reading the CSV files from S3 bucket
+NYCParking_2015 <- SparkR::read.df("s3://nycparkinghari/Parking_Violations_Issued_-_Fiscal_Year_2015.csv",source="csv",header="true",inferSchema="true")
+NYCParking_2016 <- SparkR::read.df("s3://nycparkinghari/Parking_Violations_Issued_-_Fiscal_Year_2016.csv",source="csv",header="true",inferSchema="true")
+NYCParking_2017 <- SparkR::read.df("s3://nycparkinghari/Parking_Violations_Issued_-_Fiscal_Year_2017.csv",source="csv",header="true",inferSchema="true")
+
