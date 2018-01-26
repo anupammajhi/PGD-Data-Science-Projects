97c38aaee4a56e0037f2f8c12fa4dec0b7512b45
Anupam Majhi <majhi.anupam@gmail.com>
2018-01-26 06:56:40 +0530

    Update and commit

--- 9_NYC Parking Ticket - Big Data/main.R
+++ 9_NYC Parking Ticket - Big Data/main.R
@@ -37,3 +37,18 @@ nrow(NYCParking_2017) #10803028
 
 # Looking for duplicate rows (only retaining distinct rows)
 nrow(distinct(NYCParking_2015)) #10951257 - Hence there are duplicate rows
+NYCParking_2015 <- distinct(NYCParking_2015) # Removing duplicates
+
+nrow(distinct(NYCParking_2016)) #10626899 - No duplicate rows
+nrow(distinct(NYCParking_2017)) #10803028 - No duplicate rows
+
+# Sampling of each dataset and converting it to R Data Frames. This will help us analyse the data and understand what cleaning needs to be performed.
+
+NYCParking_2015_sample <- sample(NYCParking_2015,withReplacement = F,fraction = 0.01)
+NYCParking_2015_sample_R <- collect(NYCParking_2015_sample)
+
+NYCParking_2016_sample <- sample(NYCParking_2016,withReplacement = F,fraction = 0.01) 
+NYCParking_2016_sample_R <- collect(NYCParking_2016_sample)
+
+NYCParking_2017_sample <- sample(NYCParking_2017,withReplacement = F,fraction = 0.01) 
+NYCParking_2017_sample_R <- collect(NYCParking_2017_sample)
