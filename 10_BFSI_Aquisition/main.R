b4e11e66daff851e96745257b415793fbde5bf45
Anupam Majhi <majhi.anupam@gmail.com>
2018-02-23 08:18:25 +0530

    Quick Fix

--- 10_BFSI_Aquisition/main.R
+++ 10_BFSI_Aquisition/main.R
@@ -43,3 +43,32 @@ boxplot(bank_data$age)
 
 quantile(bank_data$age,seq(0,1,0.01))
 # The age at the 99th percentile is 71
+
+# Capping the upper value of age at 71
+
+bank_data[(which(bank_data$age > 71)),]$age <- 71
+
+# Binning the age variable and storing it in variable "binning.age"
+
+bank_data$binning.age <- as.factor(cut(bank_data$age, breaks = c(16, 20, 30, 40, 50, 60, 70, 80)))
+
+# Changing the response value to numbers i.e yes = 1 and no = 0
+
+bank_data$response <- ifelse(bank_data$response == "yes", 1, 0)
+
+# Checking the numeric value of response rate in each bucket
+
+agg_age <- merge(aggregate(response ~ binning.age, bank_data, mean),aggregate(response~binning.age, bank_data, sum),by = "binning.age") 
+
+# Adding No.of_prospect
+
+count <- data.frame(table(bank_data$binning.age))
+count <- count[,-1]
+agg_age <- cbind(agg_age,count)
+colnames(agg_age) <- c("age", "response_rate", "count_prospects","No.of_prospect")
+
+# Round Off the values
+
+agg_age$response_rate <- format(round(agg_age$response_rate, 2))
+agg_age
+
