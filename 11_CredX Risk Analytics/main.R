66260a56770843550319332b95368c8618cca86a
Anupam Majhi <majhi.anupam@gmail.com>
2018-05-18 05:58:20 +0530

    Quick Fix

---
+++ 11_CredX Risk Analytics/main.R
@@ -0,0 +1,40 @@
+rm(list=ls())
+
+library(gdata)
+library(dplyr)
+library(woeBinning)
+library(caTools)
+library(ggplot2)
+library(gridExtra)
+library(DMwR)
+library(reshape2)
+library(MASS)
+library(car)
+library(caret)
+library(rpart)
+library(rattle)
+library(ROCR)
+library(DAAG)
+library(randomForest)
+# install.packages("DataExplorer")
+library(DataExplorer)
+library(knitr)
+
+dem <- read.csv("Demographic data.csv", na.strings = c("", "NA"))
+cred <- read.csv("Credit Bureau data.csv", na.strings = c("", "NA"))
+
+
+#Let's look at the structure of the data
+
+str(dem)
+str(cred)
+
+
+#== Checking Rate of Default
+
+default_rate <- sum(dem$Performance.Tag == 1, na.rm = T)/ nrow(dem)
+default_rate      
+
+# Fraction of Defaulters = 0.04134932
+
+
