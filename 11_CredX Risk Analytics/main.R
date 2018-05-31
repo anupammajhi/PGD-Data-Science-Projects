bee352eaeb271e2aa2203e7c8adb4cebe7df790e
Anupam Majhi <majhi.anupam@gmail.com>
2018-05-31 06:40:00 +0530

    Code change

--- 11_CredX Risk Analytics/main.R
+++ 11_CredX Risk Analytics/main.R
@@ -1787,10 +1787,3 @@ kable(models_dem[,c(4,1,2,3)])
 # predictions_final_only_na<-predict(dem_logistic_model_final,dem_rejects_woe, type = "response")
 # scorecard_Performance.Tag_na<-data.frame(P_Good=1-predictions_final_only_na)
 # scorecard_Performance.Tag_na<-mutate(scorecard_Performance.Tag_na, Odds_good = P_Good /(1-P_Good))
-# scorecard_Performance.Tag_na<-mutate(scorecard_Performance.Tag_na, ln_Odds = log(Odds_good))
-# scorecard_Performance.Tag_na$Original_Response <- 1
-# scorecard_Performance.Tag_na$Original_Response <- factor(as.numeric(as.character(scorecard_Performance.Tag_na$Original_Response)),levels = c(0,1))
-# scorecard_Performance.Tag_na<-mutate(scorecard_Performance.Tag_na, Score = offset+(fact*ln_Odds))
-# predicted_response_only_na<- factor(ifelse(scorecard_Performance.Tag_na$Score>=330, "0", "1"))
-# scorecard_Performance.Tag_na$Predicted_response<-predicted_response_only_na
-# conf_only_na<- confusionMatrix(predicted_response_only_na, scorecard_Performance.Tag_na$Original_Response, positive = "1")
